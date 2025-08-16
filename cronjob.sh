92ab00e5bc60ec94fa8d7e269e4d51e851e94462
OkinoLeiba <okino.leiba@yahoo.com>
2025-08-16 19:19:32 +0100

    update path to bin

--- cronjob.sh
+++ cronjob.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-0 0 1 * * /Users/om/uneness/bin/python /Users/om/uneness/unenessproj/manage.py exercise.views >> /Users/om/uneness/cronjob_log.txt 2>&1
+0 0 1 * * /Users/om/uneness/backend/bin/python /Users/om/uneness/backend/uneness/manage.py exercise.views >> /Users/om/uneness/cronjob_log.txt 2>&1
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
                                                                                                  
                                                                                                  
