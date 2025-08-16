b2f9a1a0923d1880158594ad95b703c268630aea
OkinoLeiba <okino.leiba@yahoo.com>
2025-08-16 05:12:27 +0100

    add context provider with type

--- frontend/uneness/components/LayOut.tsx
+++ frontend/uneness/components/LayOut.tsx
@@ -2,12 +2,26 @@ import React, {Component, type JSX} from 'react';
 import Header from './NavBar';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
-import HomePage from '../pages/HomePage';
 
 interface Props {
     children?: JSX.Element;
 
 }
+
+export interface StyleContextType {
+    backgroundImage: string;
+    width: number | string;
+    height: number | string;
+    minHeight: number | string;
+    backgroundPosition: string;
+    backgroundRepeat: string;
+    backgroundSize: number | string;
+    backgroundColor?: string;
+    textColor?: string;
+    padding?: number | string;
+}
+
+export const StyleContext = React.createContext<StyleContextType | null>(null);
 
 export default class Layout extends Component<Props> {
     constructor(props: Props) {
@@ -18,25 +32,36 @@ export default class Layout extends Component<Props> {
     }
 
     render() {
+        const styleContextValue: StyleContextType = {
+            backgroundImage: "url('../src/assets/images/soul-body-glow.png')",
+            width: 'auto',
+            height: '100%',
+            minHeight: '70vh',
+
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+        };
         return (
-            <>
-                <Header />
-                {/* <HomePage /> */}
-                <Outlet />
-                {this.props.children}
-                <Footer />
-            </>
+            <StyleContext.Provider value={styleContextValue}>
+                <div className={'layout-container'}>
+                    <Header />
+                    <Outlet />
+                    {this.props.children}
+                    <Footer />
+                </div>
+            </StyleContext.Provider>
         );
     }
 }
 
 // const Layout = ({ children }) => {
 //   return (
-    // <>
+    // <div>
       {/* <Header /> */}
       {/* {children} */}
       {/* <Footer /> */}
-    {/* </> */}
+    {/* </div> */}
 //   );
 // }
 // 
