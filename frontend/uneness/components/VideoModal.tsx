faba07b505d2091eeaefc64d5b5a0d6268039e71
OkinoLeiba <okino.leiba@yahoo.com>
2025-08-18 03:43:13 +0100

    update interface, add close methods, and logic to close an open modal

--- frontend/uneness/components/VideoModal.tsx
+++ frontend/uneness/components/VideoModal.tsx
@@ -1,9 +1,9 @@
-import React, { Component, MouseEventHandler } from 'react';
+import React, { Component, type MouseEventHandler } from 'react';
 import '../styles/video-modal.css';
 
 interface Props {
     videoUrl: string;
-    onClose: MouseEventHandler;
+    close?: boolean;
 }
 
 export default class VideoModal extends Component<Props> {
@@ -11,26 +11,42 @@ export default class VideoModal extends Component<Props> {
         super(props);
         this.state = {
             videoUrl: props.videoUrl || '',
-            onClose: props.onClose,
+            close: props.close || true,
         
         };
+    } 
+
+    onClose = () => {
+        this.setState(close, () => false);
     }
 
   render() {
     return (
-      <div className="video-overlay">
-        <div className="video-content">
-          <button className="close-btn" onClick={this.props.onClose}>×</button>
-          <iframe
-            width="70%"
-            height="70%"
-            src={this.props.videoUrl}
-            title="Video Player"
-            frameBorder="0"
-            allowFullScreen
-          />
-        </div>
-      </div>
+        <div className="video-overlay">
+            {close ? (
+                <div className="video-content">
+                  <button className="close-btn" onClick={this.onClose}>×</button>
+                        {/* <iframe 
+                    // width="100%"
+                    // height="100%"
+                    // src={this.props.videoUrl}
+                    // title="Video Player"
+                    // frameBorder="0"
+                    // allowFullScreen
+                    // /> */}
+                    <iframe
+                        width="100%"
+                        height="100%"
+                        src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=OGOP4KUpldhsN8mA"
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
+                    />
+                </div>
+            ): null} 
+        </div> 
     );
   }
 }
