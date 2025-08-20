97e7aecbd3fc9cb35b4f4f4d1cd086ed5289c28d
OkinoLeiba <okino.leiba@yahoo.com>
2025-08-20 21:15:37 +0100

    test additional functionality and components

--- frontend/uneness/pages/testpage.tsx
+++ frontend/uneness/pages/testpage.tsx
@@ -1,4 +1,4 @@
-import { Component, createRef} from 'react';
+import React, {createRef} from 'react';
 import Navbar from '../components/NavBar';
 import Oval from '../components/Oval';
 // import Button from '../components/button';
@@ -7,19 +7,28 @@ import Footer from '../components/Footer';
 import SelectDropdown from '../components/SelectDropdown';
 import ChatWidget from '../components/ChatWidget';
 import VideoModal from '../components/VideoModal';
-
+import ImageSlider from '../components/ImageSlider';
+import yogaImage from '../src/assets/images/group-yoga-exercise.jpg';
 import reactLogo  from '../src/assets/icons/react.svg';
 
 
 
-export default class Test extends Component {
+export default class Test extends React.Component {
     
     
     boxRef = createRef<HTMLDivElement>();
 
     state = {
-        width: 0
+        width: 0,
+        modalOpen: false,
     }
+
+     openModal = (url: string) => {
+    this.setState({ modalOpen: true, videoUrl: url })
+    };
+    closeModal = () => {
+    this.setState({ modalOpen: false });
+    };
     
     componentDidMount() {
         console.log(this.boxRef.current)
@@ -33,14 +42,14 @@ export default class Test extends Component {
         return (
             <div> 
                 <h2>Test!</h2>
-                <Navbar />
+                {/* <Navbar /> */}
                  <Oval
                     src={reactLogo}
                     alt='test'
                     elementColor={'rgba(255, 0, 0, 0.8)'}
                 />
                     
-                <Card
+                {/* <Card
                     title={'BODY'}
                     text={this.randomText}
                     btnText={'Body Button'}
@@ -50,11 +59,12 @@ export default class Test extends Component {
                     altOval={'Oval alt'}
                     accessibilityLabel={'Access Label'}
                     className={'test-card'}
-                />
-                <Footer />
-                <SelectDropdown name={'Muscle Beach'} dropdownText={''} />
-                <ChatWidget />
-                <VideoModal videoUrl={'https://youtu.be/dQw4w9WgXcQ?feature=shared'} onClose={true}/>
+                /> */}
+                {/* <Footer /> */}
+                {/* <SelectDropdown name={'Muscle Beach'} dropdownText={''} onOpen={() => ''} /> */}
+                {/* <ChatWidget /> */}
+                {/* <VideoModal videoUrl={'https://www.youtube.com/embed/dQw4w9WgXcQ?si=l9Vf1De3sL3AR4DI'} open={false} /> */}
+                <ImageSlider width={170} height={100} srcLeft={'../src/assets/images/group-yoga-exercise.jpg'} altLeft={'image-slider'}  srcRight={'../src/assets/images/pink-purple-flowers.jpg'} altRight={'image-slider'} backgroundColor='red' className={'journey-ellipse'} />
             </div>
         )
         
