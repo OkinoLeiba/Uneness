f9f8c9856e99c51d2a6709a29ca797b3359bb994
OkinoLeiba <okino.leiba@yahoo.com>
2025-08-20 21:14:57 +0100

    code clean up and add css properties via context

--- frontend/uneness/pages/JourneyPage.tsx
+++ frontend/uneness/pages/JourneyPage.tsx
@@ -1,7 +1,10 @@
-import { Component, type ErrorInfo } from 'react';
+import React, { type ErrorInfo } from 'react';
+import { StyleContext, type StyleContextType } from '../components/LayOut';
 import reactLogo from '../src/assets/icons/react.svg';
 import Oval from '../components/Oval';
-import Ellipse from '../components/Ellipse';
+//import Ellipse from '../components/Ellipse';
+import ImageSlider from '../components/ImageSlider';
+import yogaImage from '../src/assets/images/group-yoga-exercise.jpg';
 import '../styles/journey.css';
 
 
@@ -12,7 +15,7 @@ interface Props {
 
 
 
-export default class HomePage extends Component<Props>{
+export default class HomePage extends React.Component<object, Props>{
     constructor(props: Props) {
         super(props);
         this.state = {
@@ -21,7 +24,7 @@ export default class HomePage extends Component<Props>{
     }
 
 
-    journeyTet = 'Experience personalized guidance tailored to your unique needs and goals.\
+    journeyText = 'Experience personalized guidance tailored to your unique needs and goals.\
      Our holistic approach ensures every aspect of your well-being is nutured.';
     ovalAltText = 'Oval shape with icon at the center representing either mind, body, or soul.'
     pillarText = 'Discover how mind, body, and soul work together to create everlasting well-being and personal transformation.';
@@ -33,32 +36,46 @@ export default class HomePage extends Component<Props>{
 
     render() {
         return (
-            <div className={'journey-container'}> 
-                <div className={'prom-section-text'}>
-                    <h1 className={'journey-text-bold'}>Your Personal Wellness Journey</h1>
-                    <p className={'journey-text'}>{this.journeyTet}</p>
-                    <div className={'section-container'}>
-                        <Oval
-                            src={reactLogo}
-                            alt={this.ovalAltText}
-                            elementColor={'rgba(255, 0, 0, 0.8)'}
-                        />
-                        <div className={'section-text-container'}>
-                            <h3>Personalized Programs</h3>
-                            <p>Customized wellness plans designed specifically to your lifestyle and goals.</p>
+            <StyleContext.Consumer>
+                {(styles: StyleContextType) => (
+                    <div
+                        style={{
+                          backgroundImage: styles.backgroundImage,
+                          backgroundPosition: styles.backgroundPosition,
+                          backgroundRepeat: styles.backgroundRepeat,
+                          backgroundSize: styles.backgroundSize,
+                          width: styles.width,
+                          height: styles.height,
+                          minHeight: styles.minHeight,
+                          padding: styles.padding
+                        }}
+                        className={'journey-container'}>
+                        <div className={'prom-section-text'}>
+                            <h1 className={'journey-text-bold'}>Your Personal Wellness Journey</h1>
+                            <p className={'journey-text'}>{this.journeyText}</p>
+                            <div className={'section-container'}>
+                                <Oval
+                                    src={reactLogo}
+                                    alt={this.ovalAltText}
+                                    elementColor={'rgba(255, 0, 0, 0.8)'}
+                                />
+                                <div className={'section-text-container'}>
+                                    <h3>Personalized Programs</h3>
+                                    <p>Customized wellness plans designed specifically to your lifestyle and goals.</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                <div className={'ellipse-image'}>
-                    <Ellipse backgroundColor={'lightblue'} className={'journey-ellipse'} />
-                     <Oval
-                        src={reactLogo}
-                        alt={this.ovalAltText}
-                        elementColor={'rgba(255, 0, 0, 0.8)'}
-                    />
-                </div>
-            </div>
+                        <div className={'ellipse-image'}>
+                            <ImageSlider width={170} height={100} srcLeft={yogaImage} altLeft={'image-yoga-slider'}  srcRight={'../src/assets/images/pink-purple-flowers.jpg'} altRight={'image-flower-slider'} backgroundColor='red' className={'journey-ellipse'} />
+                            <Oval
+                                src={reactLogo}
+                                alt={this.ovalAltText}
+                                elementColor={'rgba(255, 0, 0, 0.8)'}
+                            />
+                        </div>
+                    </div>)}
+        </StyleContext.Consumer>
         )
     }
 }
