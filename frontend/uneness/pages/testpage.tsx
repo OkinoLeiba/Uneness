d195c587abe916ce95405a9b6587a79ba5fe05a6
OkinoLeiba <okino.leiba@yahoo.com>
2025-09-17 21:02:48 +0100

    test new button

--- frontend/uneness/pages/testpage.tsx
+++ frontend/uneness/pages/testpage.tsx
@@ -1,15 +1,17 @@
 import React, {createRef} from 'react';
 // import Navbar from '../components/NavBar';
 import Oval from '../components/Oval';
-// import Button from '../components/button';
+// import Button from '../components/Button';
+// import Button from '../components/ButtonFunc';
 // import Card from '../components/Card'
 // import Footer from '../components/Footer';
 // import SelectDropdown from '../components/SelectDropdown';
 // import ChatWidget from '../components/ChatWidget';
 // import VideoModal from '../components/VideoModal';
-import ImageSlider from '../components/ImageSlider';
+// import ImageSlider from '../components/ImageSlider';
 // import yogaImage from '../src/assets/images/group-yoga-exercise.jpg';
-import reactLogo  from '../src/assets/icons/react.svg';
+import reactLogo from '../src/assets/icons/react.svg';
+import NavDropDown from '../components/NavDropDown';
 
 
 
@@ -19,7 +21,7 @@ export default class Test extends React.Component {
     boxRef = createRef<HTMLDivElement>();
 
     state = {
-        width: 0,
+        width: 100,
         modalOpen: false,
     }
 
@@ -38,6 +40,7 @@ export default class Test extends React.Component {
         }
     }
     randomText = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui nesciunt unde eius possimus eveniet nemo, facilis rerum molestiae temporibus alias ducimus ut officiis accusantium perferendis libero officia similique quo?Amet';
+    menuItem = ['test', 'test', 'test'];
     render() {
         return (
             <div> 
@@ -47,7 +50,19 @@ export default class Test extends React.Component {
                     src={reactLogo}
                     alt='test'
                     elementColor={'rgba(255, 0, 0, 0.8)'}
-                />
+                    />
+                    {/*<Button
+                    name={`button`}
+                    form={''}
+                    formTarget={''}
+                    href={''}
+                    value={'Test'}
+                    width={this.state.width}
+                    height={this.state.width - 50}
+                    elementColor= 'red'
+                    accessibilityLabel='accessibilityLabel'
+                    className='className'
+                    /> */}
                     
                 {/* <Card
                     title={'BODY'}
@@ -64,14 +79,14 @@ export default class Test extends React.Component {
                 {/* <SelectDropdown name={'Muscle Beach'} dropdownText={''} onOpen={() => ''} /> */}
                 {/* <ChatWidget /> */}
                 {/* <VideoModal videoUrl={'https://www.youtube.com/embed/dQw4w9WgXcQ?si=l9Vf1De3sL3AR4DI'} open={false} /> */}
-                <ImageSlider width={170} height={100} srcLeft={'../src/assets/images/group-yoga-exercise.jpg'} altLeft={'image-slider'}  srcRight={'../src/assets/images/pink-purple-flowers.jpg'} altRight={'image-slider'} backgroundColor='red' className={'journey-ellipse'} />
+                {/* <ImageSlider width={170} height={100} srcLeft={'../src/assets/images/group-yoga-exercise.jpg'} altLeft={'image-slider'} srcRight={'../src/assets/images/pink-purple-flowers.jpg'} altRight={'image-slider'} backgroundColor='red' className={'journey-ellipse'} /> */}
+                <NavDropDown menuItem={this.menuItem} />
             </div>
         )
         
   }
 }
 // <Button
-    // 
 // name={`button`}
 // form={''}
 // formTarget={''}
@@ -82,4 +97,4 @@ export default class Test extends React.Component {
 // backgroundColor='red'
 // accessibilityLabel='accessibilityLabel'
 // className='className'
-{/* /> */}
+// /> 
