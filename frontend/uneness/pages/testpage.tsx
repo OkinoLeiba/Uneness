0c7617a65cf1b49cd8e4bd2f41f9bed47226018a
OkinoLeiba <okino.leiba@yahoo.com>
2025-10-08 17:22:59 +0100

    minor changes

--- frontend/uneness/pages/testpage.tsx
+++ frontend/uneness/pages/testpage.tsx
@@ -1,24 +1,28 @@
 import React, {createRef} from 'react';
 // import Navbar from '../components/NavBar';
-import Oval from '../components/Oval';
+// import Oval from '../components/Oval';
 // import Button from '../components/Button';
 // import Button from '../components/ButtonFunc';
 // import Card from '../components/Card'
+// import Card from '../components/CardFunc';
 // import Footer from '../components/Footer';
 // import SelectDropdown from '../components/SelectDropdown';
 // import ChatWidget from '../components/ChatWidget';
+// import ChatWidget from '../components/ChatWidgetFunc';
 // import VideoModal from '../components/VideoModal';
 // import ImageSlider from '../components/ImageSlider';
 // import yogaImage from '../src/assets/images/group-yoga-exercise.jpg';
-import reactLogo from '../src/assets/icons/react.svg';
+// import reactLogo from '../src/assets/icons/react.svg';
 import NavDropDown from '../components/NavDropDown';
+import Dashboard from '../components/DashBoard';
+// import Dashboard from '../components/DashBoardFunc';
 
 
 
 export default class Test extends React.Component {
     
     
-    boxRef = createRef<HTMLDivElement>();
+    boxRef: React.RefObject<HTMLDivElement | null> = createRef<HTMLDivElement>();
 
     state = {
         width: 100,
@@ -33,24 +37,24 @@ export default class Test extends React.Component {
     };
     
     componentDidMount() {
-        console.log(this.boxRef.current)
+        //console.log(this.boxRef.current)
         if (this.boxRef.current) {
         // console.log(this.boxRef.current.offsetWidth)
             this.setState({ width: this.boxRef.current.offsetWidth })
         }
     }
-    randomText = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui nesciunt unde eius possimus eveniet nemo, facilis rerum molestiae temporibus alias ducimus ut officiis accusantium perferendis libero officia similique quo?Amet';
-    menuItem = ['test', 'test', 'test'];
+    randomText: string = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui nesciunt unde eius possimus eveniet nemo, facilis rerum molestiae temporibus alias ducimus ut officiis accusantium perferendis libero officia similique quo?Amet';
+    menuItem: string[] = ['test', 'test', 'test'];
     render() {
         return (
             <div> 
                 <h2>Test!</h2>
                 {/* <Navbar /> */}
-                 <Oval
+                {/* <Oval 
                     src={reactLogo}
                     alt='test'
                     elementColor={'rgba(255, 0, 0, 0.8)'}
-                    />
+                    /> */}
                     {/*<Button
                     name={`button`}
                     form={''}
@@ -64,7 +68,7 @@ export default class Test extends React.Component {
                     className='className'
                     /> */}
                     
-                {/* <Card
+                {/*<Card
                     title={'BODY'}
                     text={this.randomText}
                     btnText={'Body Button'}
@@ -77,10 +81,11 @@ export default class Test extends React.Component {
                 /> */}
                 {/* <Footer /> */}
                 {/* <SelectDropdown name={'Muscle Beach'} dropdownText={''} onOpen={() => ''} /> */}
-                {/* <ChatWidget /> */}
+                {/* <ChatWidget />  */}
                 {/* <VideoModal videoUrl={'https://www.youtube.com/embed/dQw4w9WgXcQ?si=l9Vf1De3sL3AR4DI'} open={false} /> */}
                 {/* <ImageSlider width={170} height={100} srcLeft={'../src/assets/images/group-yoga-exercise.jpg'} altLeft={'image-slider'} srcRight={'../src/assets/images/pink-purple-flowers.jpg'} altRight={'image-slider'} backgroundColor='red' className={'journey-ellipse'} /> */}
                 <NavDropDown menuItem={this.menuItem} />
+                <Dashboard  />
             </div>
         )
         
