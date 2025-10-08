8e366560c05d6043b1466cd234e8a42c232ca95c
OkinoLeiba <okino.leiba@yahoo.com>
2025-10-08 17:19:27 +0100

    remove cookie methods and created add to separate file

--- frontend/uneness/components/LogOut.tsx
+++ frontend/uneness/components/LogOut.tsx
@@ -1,31 +1,5 @@
 import React from 'react';
 
-
-// TODO: consider creating cookie.ts
-export function deleteCookie(
-  name: string,
-  path: string = '/',
-  domain?: string
-): void {
-  const data = new Date().toUTCString();
-  let cookieStr = `${name}=; Path=${path}; Expires=${data};`
-  if (domain) {
-    cookieStr += ` Domain=${domain};`
-  }
-  document.cookie = cookieStr
-}
-
-// Remove all token; if exists
-export function clearAllCookies(): void {
-  document.cookie
-    .split(';')
-    .forEach(cookie => {
-        const name = cookie.split('=')[0].trim();
-        const data = new Date().toUTCString();
-      document.cookie = `${name}=; Path=/; Expires=${data};`;
-    });
-}
-
 export class LoggedOut extends React.Component {
 
     // handleSubmit = async (e: React.FormEvent) => {
@@ -45,12 +19,13 @@ export class LoggedOut extends React.Component {
     // }
     
     componentDidMount(): void {
-        localStorage.removeItem('token');
+      localStorage.removeItem('token');
+      window.location.href = '/homepage';
     }
-
+    
     render() {
-        // Redirect to homepage
-        //TODO: add homepage
-    return <h2>You have been logged out.</h2>;
+        // Redirect to homepage before return 
+      return <h2>You have been logged out.</h2>;
+      
   }
 }
