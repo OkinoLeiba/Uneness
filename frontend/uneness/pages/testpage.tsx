fcf04e7ecda6e2cd542b3085e84abd26d92d6f10
OkinoLeiba <okino.leiba@yahoo.com>
2025-10-13 02:47:38 +0100

    add more object in test

--- frontend/uneness/pages/testpage.tsx
+++ frontend/uneness/pages/testpage.tsx
@@ -1,21 +1,38 @@
-import React, {createRef} from 'react';
+import React, { createRef } from 'react';
+// import { PasswordChangeDone } from '../components/PasswordChangeDone';
+// import { PasswordChangeDone } from '../components/PasswordChangeDoneFunc';
+// import { PasswordChangeForm } from '../components/PasswordChangeForm';
+// import PasswordChangeForm  from '../components/PasswordChangeFormFunc';
+import { PasswordResetConfirm } from '../components/PasswordResetConfirm';
+// import { PasswordResetConfirm } from '../components/PasswordResetConfirm';
+// import reactLogo from '../src/assets/icons/react.svg';
 // import Navbar from '../components/NavBar';
 // import Oval from '../components/Oval';
+// import Oval from '../components/OvalFunc';
 // import Button from '../components/Button';
 // import Button from '../components/ButtonFunc';
 // import Card from '../components/Card'
 // import Card from '../components/CardFunc';
 // import Footer from '../components/Footer';
+// import Footer from '../components/FooterFunc.tsx';
+// import Ellipse from '../components/Ellipse.tsx';
+// import Ellipse from '../components/EllipseFunc';
 // import SelectDropdown from '../components/SelectDropdown';
 // import ChatWidget from '../components/ChatWidget';
 // import ChatWidget from '../components/ChatWidgetFunc';
 // import VideoModal from '../components/VideoModal';
 // import ImageSlider from '../components/ImageSlider';
+// import ImageSlider from '../components/ImageSliderFunc';
 // import yogaImage from '../src/assets/images/group-yoga-exercise.jpg';
 // import reactLogo from '../src/assets/icons/react.svg';
-import NavDropDown from '../components/NavDropDown';
-import Dashboard from '../components/DashBoard';
+// import NavDropDown from '../components/NavDropDown';
+// import NavDropDown from '../components/NavDropDownFunc';
+// import Dashboard from '../components/DashBoard';
 // import Dashboard from '../components/DashBoardFunc';
+// import LogIn from '../components/LogIn.tsx';
+// import LogIn from '../components/LogInFunc.tsx';
+// import SignUp from '../components/SignUp.tsx';
+// import SignUp from '../components/SignUpFunc'
 
 
 
@@ -50,9 +67,10 @@ export default class Test extends React.Component {
             <div> 
                 <h2>Test!</h2>
                 {/* <Navbar /> */}
-                {/* <Oval 
+                {/*<Oval
                     src={reactLogo}
                     alt='test'
+                    size={100}
                     elementColor={'rgba(255, 0, 0, 0.8)'}
                     /> */}
                     {/*<Button
@@ -84,8 +102,16 @@ export default class Test extends React.Component {
                 {/* <ChatWidget />  */}
                 {/* <VideoModal videoUrl={'https://www.youtube.com/embed/dQw4w9WgXcQ?si=l9Vf1De3sL3AR4DI'} open={false} /> */}
                 {/* <ImageSlider width={170} height={100} srcLeft={'../src/assets/images/group-yoga-exercise.jpg'} altLeft={'image-slider'} srcRight={'../src/assets/images/pink-purple-flowers.jpg'} altRight={'image-slider'} backgroundColor='red' className={'journey-ellipse'} /> */}
-                <NavDropDown menuItem={this.menuItem} />
-                <Dashboard  />
+                {/* <NavDropDown menuItem={this.menuItem} /> */}
+                {/* <Dashboard  /> */}
+                {/* <Ellipse 
+                    backgroundColor={'rgba(100, 100, 100, 1)'}
+                    className={'test'}
+                />*/}
+                {/* <LogIn /> */}
+                {/* <PasswordChangeDone /> */}
+                {/* <PasswordChangeForm />  */}
+                <PasswordResetConfirm />
             </div>
         )
         
