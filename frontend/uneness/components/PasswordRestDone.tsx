b918bbf0460648e1d365b321fa9a84bb0b95548c
OkinoLeiba <okino.leiba@yahoo.com>
2025-10-13 02:47:22 +0100

    code clean up remove comment

--- frontend/uneness/components/PasswordRestDone.tsx
+++ frontend/uneness/components/PasswordRestDone.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// TODO: consider factoring to functional component
 export class PasswordResetDone extends React.Component {
   render() {
     return <h2>A password reset link has been sent to your email.</h2>;
