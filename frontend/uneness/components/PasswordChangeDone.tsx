60f6c684f36b20fa52e04176d3a72d4319aa364b
OkinoLeiba <okino.leiba@yahoo.com>
2025-10-13 02:46:45 +0100

    code clean up removed comment

--- frontend/uneness/components/PasswordChangeDone.tsx
+++ frontend/uneness/components/PasswordChangeDone.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-// TODO: consider factoring to functional component
+
 export class PasswordChangeDone extends React.Component {
   render() {
     return <h2>Your password has been changed successfully.</h2>;
